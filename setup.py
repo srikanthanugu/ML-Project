5f337d58b6397b498af666cc6865fab2d8163394
srikanthanugu <anugusrikanth28@gmail.com>
2024-11-15 19:52:11 -0500

    EDA

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ def get_requirements(file_path:str)-> List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
+    return requirements
+
 setup(
     name='mlproject',
     version='0.0.1',
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from src.exceptions import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifact',"train.csv")
+    test_data_path: str=os.path.join('artifact',"test.csv")
+    raw_data_path: str=os.path.join('artifact',"data.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
